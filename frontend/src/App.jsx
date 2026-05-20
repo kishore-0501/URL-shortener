12e53459087e091efedaef917cfeecf5bef803ca
kishore-0501 <kishoreramesh5122@gmail.com>
2026-05-20 14:29:31 +0100

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,7 +68,7 @@ export default function Dashboard() {
       {/* HEADER */}
       <div className="flex items-center justify-between mb-8">
         <div>
-          <h1 className="text-4xl font-bold">URL Shortener Dashboard</h1>
+          <h1 className="text-4xl font-bold">URL Shortener Dashboard new</h1>
           <p className="text-slate-400 mt-2">
             Kubernetes + Docker + Monitoring Project
           </p>
